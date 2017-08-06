8d6328ae5dffeeb010f5f89eddda5e0ffd17d816
Dennis Burdin <dburdin@accesssoftek.com>
2017-08-06 15:53:56 +0300

    initial commit

---
+++ authorization/authorization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  authorization
+//
+//  Created by Dennis Burdin on 06.08.17.
+//  Copyright © 2017 headandhands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
