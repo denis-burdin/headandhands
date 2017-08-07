448c165983e46a1e016dfb4ce587d7523131e6dc
Dennis Burdin <dbulabs@gmail.com>
2017-08-07 12:03:35 +0300

    add weather forecast for London

--- authorization/authorization/ViewController.swift
+++ authorization/authorization/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var textFieldEmail: UITextField!
     @IBOutlet weak var buttonSignIn: UIButton!
@@ -20,14 +21,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
-        addLine(fromPoint: CGPoint(x: 15, y: 255), toPoint: CGPoint(x: 359, y: 255))
-        addLine(fromPoint: CGPoint(x: 15, y: 312), toPoint: CGPoint(x: 359, y: 312))
+        addLine(fromPoint: CGPoint(x: 15, y: textFieldEmail.frame.origin.y + textFieldEmail.frame.size.height), toPoint: CGPoint(x: 359, y: textFieldEmail.frame.origin.y + textFieldEmail.frame.size.height))
+        addLine(fromPoint: CGPoint(x: 15, y: textFieldPassword.frame.origin.y + textFieldPassword.frame.size.height), toPoint: CGPoint(x: 359, y: textFieldPassword.frame.origin.y + textFieldPassword.frame.size.height))
         
         // make round corners for sign in button
         buttonSignIn.backgroundColor = UIColor(red: 1.00, green: 0.60, blue: 0.00, alpha: 1.00)
         buttonSignIn.layer.cornerRadius = buttonSignIn.frame.size.height / 2
+        
+        // make nav bar button empty
+        let barButton = UIBarButtonItem()
+        barButton.title = ""
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = barButton
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -40,11 +46,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func actionEnter(_ sender: Any) {
-
+        
         // hide keyboard
         textFieldEmail.resignFirstResponder()
         textFieldPassword.resignFirstResponder()
-
+        
         // validate email
         let RegexEmail = "^[\\w!#$%&'*+\\-/=?\\^_`{|}~]+(\\.[\\w!#$%&'*+\\-/=?\\^_`{|}~]+)*@((([\\-\\w]+\\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\\.){3}[0-9]{1,3}))$"
         let TestResultEmail = NSPredicate.init(format:"SELF MATCHES %@",RegexEmail)
@@ -64,18 +70,32 @@ class ViewController: UIViewController {
             textFieldPassword.text = nil
         }
         
-        //if (validEmail && validPassword) {
+        if (validEmail && validPassword) {
             // show weather popup
-            let alertController = UIAlertController(title: "Weather", message: "Fuck you!", preferredStyle: UIAlertControllerStyle.alert)
-
-            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
-                (result : UIAlertAction) -> Void in
-                print("OK")
+            Alamofire.request("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1").responseJSON { response in
+                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any]
+                        let weather = json?["weather"] as! NSMutableArray
+                        let forecast = weather[0] as! NSDictionary
+                        let description = forecast["description"] as? String
+                        
+                        let alertController = UIAlertController(title: "Weather", message: description!, preferredStyle: UIAlertControllerStyle.alert)
+                        
+                        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+                            (result : UIAlertAction) -> Void in
+                            print("OK")
+                        }
+                        
+                        alertController.addAction(okAction)
+                        self.present(alertController, animated: true, completion: nil)
+                        
+                    } catch {
+                        print("Something went wrong")
+                    }
+                }
             }
-            
-            alertController.addAction(okAction)
-            self.present(alertController, animated: true, completion: nil)
-        //}
+        }
     }
     
     func addLine(fromPoint start: CGPoint, toPoint end:CGPoint) {
@@ -92,5 +112,5 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITextFieldDelegate {
-
+    
 }
